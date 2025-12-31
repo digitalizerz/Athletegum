d672669f6cd4696ed300a23e7355f2d077e5036b
Digitalizer.io <business@digitalizer.io>
2025-12-30 20:42:45 -0600

    email system sendgrid

--- app/Models/Business.php
+++ app/Models/Business.php
@@ -2,16 +2,79 @@
 
 namespace App\Models;
 
+use App\Notifications\CustomPasswordResetNotification;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 /**
- * Business model - alias for User model
- * Businesses are stored in the users table but use a separate password broker
+ * Business model - businesses are stored in the users table
+ * but use a separate password broker (businesses) for authentication
  */
-class Business extends User
+class Business extends Authenticatable
 {
-    // Business uses the same table and structure as User
-    // This allows us to have a separate password broker for businesses
+    use Notifiable;
+
+    /**
+     * Explicitly set the table name to 'users'
+     * since businesses are stored in the users table
+     */
+    protected $table = 'users';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'is_superadmin',
+        'wallet_balance',
+        'stripe_customer_id',
+        'business_name',
+        'business_information',
+        'phone',
+        'owner_principal',
+        'address_line1',
+        'address_line2',
+        'city',
+        'state',
+        'postal_code',
+        'country',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+            'is_superadmin' => 'boolean',
+            'wallet_balance' => 'decimal:2',
+        ];
+    }
+
+    /**
+     * Send the password reset notification.
+     */
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new CustomPasswordResetNotification($token));
+    }
 }
 
