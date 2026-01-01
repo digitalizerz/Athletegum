106786884faa0e1578c9fcfe1322c5433df54808
Digitalizer.io <business@digitalizer.io>
2026-01-01 12:10:46 -0600

    stripe fixes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,11 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => \App\Http\Middleware\EnsureAdmin::class,
         ]);
+        
+        // Register custom CSRF middleware to enable webhook route exclusion
+        $middleware->validateCsrfTokens(except: [
+            'stripe/webhook',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
